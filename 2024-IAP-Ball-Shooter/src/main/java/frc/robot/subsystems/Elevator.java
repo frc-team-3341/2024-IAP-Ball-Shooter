83751198b7527a1c319abbb8d64987cb83595865
Vakamudi <vakamudithejas@gmail.com>
2024-11-23 14:17:34 -0800

    initial push
    Setup everything for the elevator

---
+++ 2024-IAP-Ball-Shooter/src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+import frc.robot.RobotContainer;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+
+  Elevator elevator;
+  Joystick joystick;
+  public final WPI_TalonSRX motorE = new WPI_TalonSRX(Constants.ElevatorPorts.ElevatorPort);
+
+
+  /** Creates a new ClimbTeleop. */
+  public Elevator(Elevator elevator, Joystick joystick) {
+    this.elevator = elevator;
+    this.joystick = joystick;
+  
+  }
+  public void ElevatorUp(){
+    if (joystick.getRawButtonPressed(0)) {
+
+   }
+  }
+  public void Elevator(){
+    if (joystick.getRawButtonPressed(0)) {
+      
+   }
+  }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double yValue = joystick.getY()
+ 
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
